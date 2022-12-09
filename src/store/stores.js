cb7d7817a7b02ade49083f6453be799066899291
Stukova Olya <stukova.o@gmail.com>
2022-12-09 15:49:34 +0500

    Initiate stores once in Providers

--- src/store/stores.js
+++ src/store/stores.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import ConfigStore from 'store/config';
 import ClusteringStore from 'store/clustering';
@@ -22,62 +22,71 @@ export const UiRoriStoreContext = React.createContext();
 export const VisualizationStoreContext = React.createContext();
 export const WebworkerStoreContext = React.createContext();
 
+// https://reactjs.org/docs/hooks-faq.html#how-to-create-expensive-objects-lazily
+function getStore(Store) {
+  const ref = useRef(null);
+  if (ref.current === null) {
+    ref.current = new Store();
+  }
+  return ref.current;
+}
+
 export const ConfigProvider = ({ children }) => (
-  <ConfigStoreContext.Provider value={new ConfigStore()}>
+  <ConfigStoreContext.Provider value={getStore(ConfigStore)}>
     {children}
   </ConfigStoreContext.Provider>
 );
 
 export const ClusteringProvider = ({ children }) => (
-  <ClusteringStoreContext.Provider value={new ClusteringStore()}>
+  <ClusteringStoreContext.Provider value={getStore(ClusteringStore)}>
     {children}
   </ClusteringStoreContext.Provider>
 );
 
 export const DataProvider = ({ children }) => (
-  <DataStoreContext.Provider value={new DataStore()}>
+  <DataStoreContext.Provider value={getStore(DataStore)}>
     {children}
   </DataStoreContext.Provider>
 );
 
 export const LayoutProvider = ({ children }) => (
-  <LayoutStoreContext.Provider value={new LayoutStore()}>
+  <LayoutStoreContext.Provider value={getStore(LayoutStore)}>
     {children}
   </LayoutStoreContext.Provider>
 );
 
 export const NormalizationProvider = ({ children }) => (
-  <NormalizationStoreContext.Provider value={new NormalizationStore()}>
+  <NormalizationStoreContext.Provider value={getStore(NormalizationStore)}>
     {children}
   </NormalizationStoreContext.Provider>
 );
 
 export const QueryStringProvider = ({ children }) => (
-  <QueryStringStoreContext.Provider value={new QueryStringStore()}>
+  <QueryStringStoreContext.Provider value={getStore(QueryStringStore)}>
     {children}
   </QueryStringStoreContext.Provider>
 );
 
 export const UiProvider = ({ children }) => (
-  <UiStoreContext.Provider value={new UiStore()}>
+  <UiStoreContext.Provider value={getStore(UiStore)}>
     {children}
   </UiStoreContext.Provider>
 );
 
 export const UiRoriProvider = ({ children }) => (
-  <UiRoriStoreContext.Provider value={new UiRoriStore()}>
+  <UiRoriStoreContext.Provider value={getStore(UiRoriStore)}>
     {children}
   </UiRoriStoreContext.Provider>
 );
 
 export const VisualizationProvider = ({ children }) => (
-  <VisualizationStoreContext.Provider value={new VisualizationStore()}>
+  <VisualizationStoreContext.Provider value={getStore(VisualizationStore)}>
     {children}
   </VisualizationStoreContext.Provider>
 );
 
 export const WebworkerProvider = ({ children }) => (
-  <WebworkerStoreContext.Provider value={new WebworkerStore()}>
+  <WebworkerStoreContext.Provider value={getStore(WebworkerStore)}>
     {children}
   </WebworkerStoreContext.Provider>
 );
